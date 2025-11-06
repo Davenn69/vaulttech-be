6403034a582f0dcb8c479a4d28df4741b57729b7
Davenn69 <devendeven98@gmail.com>
2025-11-06 15:08:55 +0700

    added : logger function

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express"
+import logger from "./middlewares/logger"
 
 const app = express()
+
+app.use(logger)
 
 export default app
---
+++ src/middlewares/logger.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function logger(req: Request, res: Response, next: NextFunction) {
+    console.log(`Request Method ${req.method}`)
+    console.log(`Request Body ${req.body}`)
+    console.log(`Request Url ${req.url}`)
+    next()
+}
