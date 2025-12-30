2edb529ead20e23ffffe321c5ec49a99b38255dd
Davenn69 <devendeven98@gmail.com>
2025-12-30 09:19:41 +0700

    added : error and success messages

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -1,8 +1,21 @@
 class ErrorMessages {
     missingBody: string = "missing body parameter"
+
     emailMissing: string = "missing email parameter"
+    emailCheck: string = "email must be valid"
+
     passwordMissing: string = "missing password parameter"
+    passwordLength: string = "password must have 30 characters or less"
+    passwordCheck: string = "password must have 1 uppercase, 1 lowercase, 1 number, 1 special char"
+
     usernameMissing: string = "missing username parameter"
+    usernameLength: string = "username must have 30 characters or less"
+
+    tokenMissing: string = "token is not provided"
+    invalidUser: string = "invalid user"
+    fileMissing: string = "missing file parameter"
+
+    folderIdMissing: string = "missing folder id parameter"
 }
 
 export const errors = new ErrorMessages()
--- src/utils/successMessages.ts
+++ src/utils/successMessages.ts
@@ -1,5 +1,8 @@
 class SuccessMessages {
     register: string = "register user successful"
+    login: string = "login successful"
+    successUpload = 'file has been successfully uploaded'
+    successRetrieveFiles = 'successfully retrieved files'
 }
 
 export const successMessages = new SuccessMessages()
