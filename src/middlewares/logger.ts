0fff9faa79e9569c0b683a13f6c105c34480f847
Davenn69 <devendeven98@gmail.com>
2025-12-30 09:18:26 +0700

    modified : logger

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export default function logger(req: Request, res: Response, next: NextFunction) {
     console.log(`Request Method ${req.method}`)
-    console.log(`Request Body ${req.body}`)
+    console.log(`Request Body `, req.body)
     console.log(`Request Url ${req.url}`)
     next()
 }
