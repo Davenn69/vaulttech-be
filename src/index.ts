9560850225d45345dc47154db1cbb008a2b1e677
Davenn69 <devendeven98@gmail.com>
2026-01-08 15:55:06 +0700

    added : connection string setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import postgres from "postgres";
 import app from "./app";
 import dotenv from "dotenv"
+import { drizzle } from "drizzle-orm/singlestore/driver";
 
 dotenv.config()
 const PORT = process.env.PORT
+const connectionString = process.env.DATABASE_URL!
+
+const client = postgres(connectionString)
+export const db = drizzle(client)
+
 app.listen(PORT, () => {
     console.log(`Server is running in PORT ${PORT}`)
 })
