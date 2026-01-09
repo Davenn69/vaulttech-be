8d919d01c8aa0af9b05c918745e1c371b540c7bc
Davenn69 <devendeven98@gmail.com>
2026-01-09 09:42:14 +0700

    modified : drizzle package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import postgres from "postgres";
 import app from "./app";
 import dotenv from "dotenv"
-import { drizzle } from "drizzle-orm/singlestore/driver";
+import { drizzle } from "drizzle-orm/postgres-js";
 
 dotenv.config()
 const PORT = process.env.PORT
 const connectionString = process.env.DATABASE_URL!
+
+console.log(connectionString)
 
 const client = postgres(connectionString)
 export const db = drizzle(client)
