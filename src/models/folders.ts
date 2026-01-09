ed2f778a148c11aa27b94f1b38a305e5ecd8a490
Davenn69 <devendeven98@gmail.com>
2026-01-09 09:42:39 +0700

    modified : folders model

--- src/models/folders.ts
+++ src/models/folders.ts
@@ -2,14 +2,14 @@ import { boolean, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
 import { authUsers } from "./authSchema";
 
 export const folders = pgTable('folders', {
-    id: uuid('id').primaryKey(),
+    id: uuid('id').defaultRandom().primaryKey(),
     userId: uuid('user_id').notNull().references(() => authUsers.id,),
     name: varchar('name').notNull(),
-    createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).notNull(),
-    updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' }).notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' }),
     createdBy: varchar('created_by').notNull(),
     updatedBy: varchar('updated_by'),
     path: varchar('path').notNull(),
-    isFavourite: boolean('is_favourite').notNull(),
-    isDeleted: boolean('is_deleted').notNull(),
+    isFavourite: boolean('is_favourite').default(false).notNull(),
+    isDeleted: boolean('is_deleted').default(false).notNull(),
 })
