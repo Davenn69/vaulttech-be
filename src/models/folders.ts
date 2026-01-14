15a424ef7891116490f51a8ca8048f7729ab364d
Davenn69 <devendeven98@gmail.com>
2026-01-14 09:38:52 +0700

    modified : parent id reference

--- src/models/folders.ts
+++ src/models/folders.ts
@@ -1,9 +1,10 @@
-import { boolean, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, foreignKey, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { authUsers } from "./authSchema";
 
 export const folders = pgTable('folders', {
     id: uuid('id').defaultRandom().primaryKey(),
     userId: uuid('user_id').notNull().references(() => authUsers.id,),
+    parentId: uuid('parent_id'),
     name: varchar('name').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
     updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' }),
@@ -12,4 +13,12 @@ export const folders = pgTable('folders', {
     path: varchar('path').notNull(),
     isFavourite: boolean('is_favourite').default(false).notNull(),
     isDeleted: boolean('is_deleted').default(false).notNull(),
+}, (table) => {
+    return {
+        parentReference: foreignKey({
+            columns: [table.parentId],
+            foreignColumns: [table.id],
+            name: "folders_parent_id_fkey"
+        })
+    }
 })
