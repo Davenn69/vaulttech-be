daba92c7663d1fe3443b6e92b17296d7400b174a
Davenn69 <devendeven98@gmail.com>
2026-01-15 14:39:26 +0700

    modified : authHandlers with prettier

--- src/handlers/authHandlers.ts
+++ src/handlers/authHandlers.ts
@@ -8,50 +8,77 @@ import { db } from "..";
 import { profiles } from "../models/profiles";
 import { folders } from "../models/folders";
 
-export const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        if (!req.body) return next(new CustomError(errors.missingBody, 400))
-        const { email, password, username } = req.body
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.body) return next(new CustomError(errors.missingBody, 400));
+    const { email, password, username } = req.body;
 
-        const emailMessage = validation.validateEmail(email)
-        if (emailMessage) return next(new CustomError(emailMessage, 400))
+    const emailMessage = validation.validateEmail(email);
+    if (emailMessage) return next(new CustomError(emailMessage, 400));
 
-        const passwordMessage = validation.validatePassword(password)
-        if (passwordMessage) return next(new CustomError(passwordMessage, 400))
+    const passwordMessage = validation.validatePassword(password);
+    if (passwordMessage) return next(new CustomError(passwordMessage, 400));
 
-        const usernameMessage = validation.validateUsername(username)
-        if (usernameMessage) return next(new CustomError(usernameMessage, 400))
+    const usernameMessage = validation.validateUsername(username);
+    if (usernameMessage) return next(new CustomError(usernameMessage, 400));
 
-        const { data: data, error: error } = await supabase.auth.signUp({ email: email, password: password })
-        if (error) return next(new CustomError(error.message, 400))
+    await db.transaction(async (tx) => {
+      const { data: signUpData, error: signUpError } =
+        await supabase.auth.signUp({ email: email, password: password });
+      if (signUpError) return next(new CustomError(signUpError.message, 400));
 
-        await db.transaction(async (tx) => {
-            const [folder] = await tx.insert(folders).values({ name: 'Home', createdBy: username, path: '/', userId: data.user?.id! }).returning()
-
-            if (!folder) return next(new CustomError('error creating folder', 400))
-
-            const profile = await tx.insert(profiles).values({ id: data.user?.id!, username: username, homeFolderId: folder.id }).returning()
+      const [folder] = await tx
+        .insert(folders)
+        .values({
+          name: "Home",
+          createdBy: username,
+          path: "/",
+          userId: signUpData.user?.id!,
         })
+        .returning();
 
-        return res.status(201).json({ message: successMessages.register, data: data })
-    } catch (e: any) {
-        next(new CustomError(e.message, e.status))
-    }
-}
+      if (!folder) return next(new CustomError("error creating folder", 400));
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        if (!req.body) return next(new CustomError(errors.missingBody, 400))
-        const { email, password } = req.body
+      await tx
+        .insert(profiles)
+        .values({
+          id: signUpData.user?.id!,
+          username: username,
+          homeFolderId: folder.id,
+        });
 
-        if (!email) return next(new CustomError(errors.emailMissing, 400))
-        if (!password) return next(new CustomError(errors.passwordMissing, 400))
+      return res
+        .status(201)
+        .json({ message: successMessages.register, data: signUpData });
+    });
+  } catch (e: any) {
+    next(new CustomError(e.message, e.status));
+  }
+};
 
-        const { data: data, error: error } = await supabase.auth.signInWithPassword({ email: email, password: password })
-        if (error) return next(new CustomError(error.message, 400))
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.body) return next(new CustomError(errors.missingBody, 400));
+    const { email, password } = req.body;
 
-        return res.status(200).json({ message: successMessages.login, data: data })
-    } catch (e: any) {
-        next(new CustomError(e.message))
-    }
-}
+    if (!email) return next(new CustomError(errors.emailMissing, 400));
+    if (!password) return next(new CustomError(errors.passwordMissing, 400));
+
+    const { data: data, error: error } = await supabase.auth.signInWithPassword(
+      { email: email, password: password }
+    );
+    if (error) return next(new CustomError(error.message, 400));
+
+    return res.status(200).json({ message: successMessages.login, data: data });
+  } catch (e: any) {
+    next(new CustomError(e.message));
+  }
+};
