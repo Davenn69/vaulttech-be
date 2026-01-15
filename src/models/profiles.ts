80b7f34721971fa1d5f6227dae81b7803a8f461c
Davenn69 <devendeven98@gmail.com>
2026-01-15 15:11:23 +0700

    modified : auth schema file name

--- src/models/profiles.ts
+++ src/models/profiles.ts
@@ -1,11 +1,19 @@
-import { pgTable, uuid, varchar, boolean, timestamp } from "drizzle-orm/pg-core";
-import { authUsers } from "./authSchema";
+import {
+  pgTable,
+  uuid,
+  varchar,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { authUsers } from "./auth";
 import { folders } from "./folders";
 
-export const profiles = pgTable('profiles', {
-    id: uuid('id').primaryKey().references(() => authUsers.id),
-    username: varchar('username').notNull(),
-    isActive: boolean('is_active').notNull().default(true),
-    deletedAt: timestamp('deleted_at', { mode: 'string' }),
-    homeFolderId: uuid('home_folder_id').references(() => folders.id),
-})
+export const profiles = pgTable("profiles", {
+  id: uuid("id")
+    .primaryKey()
+    .references(() => authUsers.id),
+  username: varchar("username").notNull(),
+  isActive: boolean("is_active").notNull().default(true),
+  deletedAt: timestamp("deleted_at", { mode: "string" }),
+  homeFolderId: uuid("home_folder_id").references(() => folders.id),
+});
