3dedc869561aedd25555ebd54668511f21ffd7c2
Davenn69 <devendeven98@gmail.com>
2026-05-06 15:21:34 +0700

    modified : word handlers

--- src/handlers/wordHandlers.ts
+++ src/handlers/wordHandlers.ts
@@ -17,7 +17,7 @@ import {
   convertWordDocumentXmlToTiptap,
   createBlankWordDocument,
   createWordDocumentFromTiptap,
-  extractWordDocumentXml,
+  extractWordDocumentParts,
 } from "../utils/wordUtils";
 
 const buildFileHash = (buffer: Buffer) => {
@@ -78,6 +78,9 @@ export const createWordFile = async (
           upsert: false,
         });
 
+      console.log(filePath);
+      console.log(bucketError);
+
       if (bucketError)
         throw new CustomError(bucketError.message, HttpStatusCode.BAD_REQUEST);
 
@@ -120,10 +123,7 @@ export const createWordFile = async (
 
         return res.status(HttpStatusCode.CREATED).json({
           message: successMessages.successCreateWordFile,
-          data: {
-            file: fileData,
-            revision: revisionData,
-          },
+          data: fileData,
         });
       } catch (error) {
         try {
@@ -192,8 +192,11 @@ export const getWordFile = async (
         );
 
       const buffer = Buffer.from(await bucketFile.arrayBuffer());
-      const documentXml = extractWordDocumentXml(buffer);
-      const tiptapContent = convertWordDocumentXmlToTiptap(documentXml);
+      const { documentXml, numberingXml } = extractWordDocumentParts(buffer);
+      const tiptapContent = convertWordDocumentXmlToTiptap(
+        documentXml,
+        numberingXml,
+      );
 
       return res.status(HttpStatusCode.OK).json({
         message: successMessages.successRetrieveWordFile,
