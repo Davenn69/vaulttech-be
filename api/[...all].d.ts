1af53d178ef4b21b3266c831f4181e01224f5050
Davenn69 <devendeven98@gmail.com>
2026-06-03 21:40:05 +0700

    removed : duplicate

--- api/[...all].d.ts
+++
@@ -1,3 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-export default function handler(req: IncomingMessage, res: ServerResponse): any;
-//# sourceMappingURL=%5B...all%5D.d.ts.map
