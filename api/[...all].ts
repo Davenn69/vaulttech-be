fc05c30e377cb690d0001f921cf4c632522e16f9
Davenn69 <devendeven98@gmail.com>
2026-06-03 22:13:39 +0700

    modified : strucutre

--- api/[...all].ts
+++
@@ -1,9 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import app from "../src/app";
-
-export default function handler(
-  req: IncomingMessage,
-  res: ServerResponse,
-) {
-  return app(req, res);
-}
